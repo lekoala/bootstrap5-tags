f46c4dbe80285873d5c4ed50a4556e9dc4f07f84
Thomas <thomas@lekoala.be>
2021-05-04 22:27:35 +0200

    init

---
+++ readme.md
@@ -0,0 +1,45 @@
+# bootstrap5-tags
+
+[![NPM](https://nodei.co/npm/bootstrap5-tags.png?mini=true)](https://nodei.co/npm/bootstrap5-tags/) 
+[![Downloads](https://img.shields.io/npm/dt/bootstrap5-tags.svg)](https://www.npmjs.com/package/bootstrap5-tags)
+
+## How to use
+
+An ES6 native replacement for `select` using standards Bootstrap 5 styles.
+
+No additional CSS needed! Supports creation of new tags.
+
+```js
+import Tags from "./tags.js";
+Tags.init();
+```
+
+By default, only provided options are available. Validation error
+will be displayed in case of invalid tag.
+
+```html
+<label for="validationTags" class="form-label">Tags</label>
+<select class="form-select" id="validationTags" multiple>
+    <option selected disabled hidden value="">Choose a tag...</option>
+    <option value="1" selected="selected">Apple</option>
+    <option value="2">Banana</option>
+    <option value="3">Orange</option>
+</select>
+<div class="invalid-feedback">Please select a valid tag.</div>
+```
+
+Use attribute `data-allow-new` to allow creation of new tags. Their
+default value will be equal to the text. Since you can enter
+arbitrary text, no validation will occur.
+
+```html
+<select class="form-select" id="validationTags" multiple data-allow-new="true">
+```
+
+## Demo
+
+https://codepen.io/lekoalabe/pen/ExWYEqx
+
+## How does it look ?
+
+![screenshot](screenshot.png "screenshot")
---
+++ tags.js
@@ -0,0 +1,400 @@
+/**
+ * Bootstrap 5 tags
+ *
+ * Turns your select[multiple] into nice tags lists
+ *
+ * Required Bootstrap 5 styles:
+ * - badge
+ * - background-color utility
+ * - margin-end utility
+ * - forms
+ * - dropdown
+ */
+
+const ACTIVE_CLASS = "bg-light";
+const VALUE_ATTRIBUTE = "data-value";
+
+class Tags {
+  /**
+   * @param {HTMLSelectElement} selectElement
+   */
+  constructor(selectElement) {
+    this.selectElement = selectElement;
+    this.selectElement.style.display = "none";
+    this.placeholder = this.getPlaceholder();
+    this.allowNew = selectElement.dataset.allowNew ? true : false;
+
+    // Create elements
+    this.holderElement = document.createElement("div");
+    this.containerElement = document.createElement("div");
+    this.dropElement = document.createElement("ul");
+    this.searchInput = document.createElement("input");
+
+    this.holderElement.appendChild(this.containerElement);
+    this.containerElement.appendChild(this.searchInput);
+    this.holderElement.appendChild(this.dropElement);
+    // insert after
+    this.selectElement.parentNode.insertBefore(this.holderElement, this.selectElement.nextSibling);
+
+    // Configure them
+    this.configureSearchInput();
+    this.configureHolderElement();
+    this.configureDropElement();
+    this.configureContainerElement();
+    this.buildSuggestions();
+  }
+
+  /**
+   * Attach to all elements matched by the selector
+   * @param {string} selector
+   */
+  static init(selector = "select[multiple]") {
+    let list = document.querySelectorAll(selector);
+    for (let i = 0; i < list.length; i++) {
+      let el = list[i];
+      let inst = new Tags(el);
+    }
+  }
+
+  /**
+   * @returns {string}
+   */
+  getPlaceholder() {
+    let firstOption = this.selectElement.querySelector("option");
+    if (!firstOption) {
+      return;
+    }
+    if (!firstOption.value) {
+      let placeholder = firstOption.innerText;
+      firstOption.remove();
+      return placeholder;
+    }
+    if (this.selectElement.getAttribute("placeholder")) {
+      return this.selectElement.getAttribute("placeholder");
+    }
+    if (this.selectElement.getAttribute("data-placeholder")) {
+      return this.selectElement.getAttribute("data-placeholder");
+    }
+    return "";
+  }
+
+  configureDropElement() {
+    this.dropElement.classList.add("dropdown-menu");
+  }
+
+  configureHolderElement() {
+    this.holderElement.classList.add("form-control");
+    this.holderElement.classList.add("dropdown");
+  }
+
+  configureContainerElement() {
+    this.containerElement.addEventListener("click", (event) => {
+      this.searchInput.focus();
+    });
+
+    // add initial values
+    let initialValues = this.selectElement.querySelectorAll("option[selected]");
+    for (let j = 0; j < initialValues.length; j++) {
+      let initialValue = initialValues[j];
+      if (!initialValue.value) {
+        continue;
+      }
+      this.addItem(initialValue.innerText, initialValue.value);
+    }
+  }
+
+  configureSearchInput() {
+    this.searchInput.type = "text";
+    this.searchInput.autocomplete = false;
+    this.searchInput.style.border = 0;
+    this.searchInput.style.outline = 0;
+    this.searchInput.style.maxWidth = "100%";
+
+    this.adjustWidth();
+
+    this.searchInput.addEventListener("input", (event) => {
+      this.adjustWidth();
+      if (this.searchInput.value.length >= 1) {
+        this.showSuggestions();
+      } else {
+        this.hideSuggestions();
+      }
+    });
+    // keypress doesn't send arrow keys
+    this.searchInput.addEventListener("keydown", (event) => {
+      if (event.code == "Enter") {
+        let selection = this.getActiveSelection();
+        if (selection) {
+          this.addItem(selection.innerText, selection.getAttribute(VALUE_ATTRIBUTE));
+          this.resetSearchInput();
+          this.hideSuggestions();
+        } else {
+          // We use what is typed
+          if (this.allowNew) {
+            this.addItem(this.searchInput.value);
+            this.resetSearchInput();
+            this.hideSuggestions();
+          }
+        }
+        event.preventDefault();
+        return;
+      }
+      if (event.code == "ArrowUp") {
+        this.moveSelectionUp();
+      }
+      if (event.code == "ArrowDown") {
+        this.moveSelectionDown();
+      }
+      if (event.code == "Backspace") {
+        if (this.searchInput.value.length == 0) {
+          this.removeLastItem();
+          this.adjustWidth();
+        }
+      }
+    });
+  }
+
+  moveSelectionUp() {
+    let active = this.getActiveSelection();
+    if (active) {
+      let prev = active.parentNode;
+      do {
+        prev = prev.previousSibling;
+      } while (prev && prev.style.display == "none");
+      if (!prev) {
+        return;
+      }
+      active.classList.remove(ACTIVE_CLASS);
+      prev.querySelector("a").classList.add(ACTIVE_CLASS);
+    }
+  }
+
+  moveSelectionDown() {
+    let active = this.getActiveSelection();
+    if (active) {
+      let next = active.parentNode;
+      do {
+        next = next.nextSibling;
+      } while (next && next.style.display == "none");
+      if (!next) {
+        return;
+      }
+      active.classList.remove(ACTIVE_CLASS);
+      next.querySelector("a").classList.add(ACTIVE_CLASS);
+    }
+  }
+
+  /**
+   * Adjust the field to fit its content
+   */
+  adjustWidth() {
+    if (this.searchInput.value) {
+      this.searchInput.size = this.searchInput.value.length + 1;
+    } else {
+      // Show the placeholder only if empty
+      if (this.getSelectedValues().length) {
+        this.searchInput.placeholder = "";
+        this.searchInput.size = 1;
+      } else {
+        this.searchInput.size = this.placeholder.length;
+        this.searchInput.placeholder = this.placeholder;
+      }
+    }
+  }
+
+  /**
+   * Add suggestions from element
+   */
+  buildSuggestions() {
+    let options = this.selectElement.querySelectorAll("option");
+    for (let i = 0; i < options.length; i++) {
+      let opt = options[i];
+      if (!opt.getAttribute("value")) {
+        continue;
+      }
+      let newChild = document.createElement("li");
+      let newChildLink = document.createElement("a");
+      newChild.append(newChildLink);
+      newChildLink.classList.add("dropdown-item");
+      newChildLink.setAttribute(VALUE_ATTRIBUTE, opt.getAttribute("value"));
+      newChildLink.setAttribute("href", "#");
+      newChildLink.innerText = opt.innerText;
+      this.dropElement.appendChild(newChild);
+
+      // Hover sets active item
+      newChildLink.addEventListener("mouseenter", (event) => {
+        this.removeActiveSelection();
+        newChild.querySelector("a").classList.add(ACTIVE_CLASS);
+      });
+
+      newChildLink.addEventListener("click", (event) => {
+        event.preventDefault();
+        this.addItem(newChildLink.innerText, newChildLink.getAttribute(VALUE_ATTRIBUTE));
+        this.resetSearchInput();
+        this.hideSuggestions();
+      });
+    }
+  }
+
+  resetSearchInput() {
+    this.searchInput.value = "";
+    this.adjustWidth();
+  }
+
+  /**
+   * @returns {array}
+   */
+  getSelectedValues() {
+    let selected = this.selectElement.querySelectorAll("option:checked");
+    return Array.from(selected).map((el) => el.value);
+  }
+
+  /**
+   * The element create with buildSuggestions
+   */
+  showSuggestions() {
+    if (!this.dropElement.classList.contains("show")) {
+      this.dropElement.classList.add("show");
+    }
+
+    // Position next to search input
+    this.dropElement.style.left = this.searchInput.offsetLeft + "px";
+
+    // Get search value
+    let search = this.searchInput.value.toLocaleLowerCase();
+
+    // Get current values
+    let values = this.getSelectedValues();
+
+    // Filter the list according to search string
+    let list = this.dropElement.querySelectorAll("li");
+    let found = false;
+    let firstItem = null;
+    for (let i = 0; i < list.length; i++) {
+      let item = list[i];
+      let text = item.innerText.toLocaleLowerCase();
+      let link = item.querySelector("a");
+
+      // Remove previous selection
+      link.classList.remove(ACTIVE_CLASS);
+
+      // Hide selected values
+      if (values.indexOf(link.getAttribute(VALUE_ATTRIBUTE)) != -1) {
+        item.style.display = "none";
+        continue;
+      }
+
+      if (text.indexOf(search) !== -1) {
+        item.style.display = "list-item";
+        found = true;
+        if (!firstItem) {
+          firstItem = item;
+        }
+      } else {
+        item.style.display = "none";
+      }
+    }
+
+    // Special case if nothing matches
+    if (!found) {
+      this.dropElement.classList.remove("show");
+    }
+
+    // Always select first item
+    if (firstItem) {
+      if (this.holderElement.classList.contains("is-invalid")) {
+        this.holderElement.classList.remove("is-invalid");
+      }
+      firstItem.querySelector("a").classList.add(ACTIVE_CLASS);
+    } else {
+      // No item and we don't allow new items => error
+      if (!this.allowNew) {
+        this.holderElement.classList.add("is-invalid");
+      }
+    }
+  }
+
+  /**
+   * The element create with buildSuggestions
+   */
+  hideSuggestions(dropEl) {
+    if (this.dropElement.classList.contains("show")) {
+      this.dropElement.classList.remove("show");
+    }
+    if (this.holderElement.classList.contains("is-invalid")) {
+      this.holderElement.classList.remove("is-invalid");
+    }
+  }
+
+  /**
+   * @returns {HTMLElement}
+   */
+  getActiveSelection() {
+    return this.dropElement.querySelector("a." + ACTIVE_CLASS);
+  }
+
+  removeActiveSelection() {
+    let selection = this.getActiveSelection();
+    if (selection) {
+      selection.classList.remove(ACTIVE_CLASS);
+    }
+  }
+
+  removeLastItem() {
+    let items = this.containerElement.querySelectorAll("span");
+    if (!items.length) {
+      return;
+    }
+    let lastItem = items[items.length - 1];
+    this.removeItem(lastItem.getAttribute(VALUE_ATTRIBUTE));
+  }
+
+  /**
+   * @param {string} text
+   * @param {string} value
+   */
+  addItem(text, value) {
+    if (!value) {
+      value = text;
+    }
+    let span = document.createElement("span");
+    span.classList.add("badge");
+    span.classList.add("bg-primary");
+    span.classList.add("me-2");
+    span.setAttribute(VALUE_ATTRIBUTE, value);
+    span.innerText = text;
+    this.containerElement.insertBefore(span, this.searchInput);
+
+    // update select
+    let opt = this.selectElement.querySelector('option[value="' + value + '"]');
+    if (opt) {
+      opt.setAttribute("selected", "selected");
+    } else {
+      // we need to create a new option
+      opt = document.createElement("option");
+      opt.value = value;
+      opt.innerText = text;
+      opt.setAttribute("selected", "selected");
+      this.selectElement.appendChild(opt);
+    }
+  }
+
+  /**
+   * @param {string} value
+   */
+  removeItem(value) {
+    let item = this.containerElement.querySelector("span[" + VALUE_ATTRIBUTE + '="' + value + '"]');
+    if (!item) {
+      return;
+    }
+    item.remove();
+
+    // update select
+    let opt = this.selectElement.querySelector('option[value="' + value + '"]');
+    if (opt) {
+      opt.removeAttribute("selected");
+    }
+  }
+}
+
+export default Tags;
